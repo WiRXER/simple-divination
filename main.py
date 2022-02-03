4c2f206ac1f6dbc8baa048f14c56ed2cdd462466
WiRXER <34344409+WiRXER@users.noreply.github.com>
2022-02-03 18:10:34 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+import random
+import os
+
+items = [
+    'Yes', 'No',
+    'Rather yes', 'Rather no',
+    'I dont know'
+]
+
+def start():
+    _start = True
+    while _start:
+        random.shuffle(items)
+
+        for i in items:
+            if items.index(i) < 10:
+                print(f'''
+|-----|
+|     |
+|  {items.index(i)}  |
+|     |
+|-----|
+        ''')
+            else:
+                print(f'''
+|-----|
+|     |
+| {items.index(i)}  |
+|     |
+|-----|
+        ''')
+        
+        choice = int(input('Your choice --> '))
+
+        if choice < 0 or choice > len(items) - 1:
+            print(f'\u001b[31;1mMust choice a number 0 - {len(items) - 1}')
+        else:
+            print(f'\u001b[37;1mYour result: {items[choice]}')
+            _start = False
+
+
+print('''
+
+\u001b[33;1mThis is a simple divination program.
+\u001b[32;1m
+[1] - start
+[2] - quit
+
+''')
+
+c = int(input('\u001b[37;1mchoice --> '))
+if c == 1:
+    os.system('clear||cls')
+    start()
+elif c == 2:
+    quit()
+else:
+    print('\u001b[31;1mWrong number')
